6aedc144829e3121ce324becfb9d53c3cdabedb0
G K <gk@setup.com>
2024-08-23 21:17:27 -0400

    Include dist

---
+++ dist/types/interfaces/Size.d.ts
@@ -0,0 +1,15 @@
+export declare const Size: {
+    readonly LETTER: readonly [279, 216];
+    readonly A2: readonly [594, 420];
+    readonly A3: readonly [420, 297];
+    readonly A4: readonly [297, 210];
+    readonly A5: readonly [210, 148];
+    readonly A6: readonly [148, 105];
+    readonly B2: readonly [707, 500];
+    readonly B3: readonly [500, 353];
+    readonly B4: readonly [353, 250];
+    readonly B5: readonly [250, 176];
+    readonly B6: readonly [176, 125];
+};
+export type SizeType = (typeof Size)[keyof typeof Size];
+//# sourceMappingURL=Size.d.ts.map
